060f9822190a121f89817be8a9d552c053b452a2
Xiao liu <46879761+liangzai006@users.noreply.github.com>
2025-09-02 15:14:53 +0800

    feat: support coreshub deploy 2

--- cmd/kk/pkg/plugins/dns/templates/coredns_configmap.go
+++ cmd/kk/pkg/plugins/dns/templates/coredns_configmap.go
@@ -17,9 +17,14 @@
 package templates
 
 import (
+	"fmt"
+	"strings"
+	"text/template"
+
+	"github.com/kubesphere/kubekey/v3/cmd/kk/pkg/common"
+	"github.com/kubesphere/kubekey/v3/cmd/kk/pkg/core/connector"
 	"github.com/kubesphere/kubekey/v3/cmd/kk/pkg/utils"
 	"github.com/lithammer/dedent"
-	"text/template"
 )
 
 var (
@@ -95,3 +100,29 @@ data:
 
     `)))
 )
+
+func GenerateDnsHosts(runtime connector.ModuleRuntime, kubeConf *common.KubeConf) string {
+
+	var aiUrl, apiUrl string
+
+	if kubeConf.Cluster.ControlPlaneEndpoint.Address != "" {
+		aiUrl = fmt.Sprintf("%s %s", kubeConf.Cluster.ControlPlaneEndpoint.Address, fmt.Sprintf("ai.%s", kubeConf.Cluster.Aicp.Domain))
+		apiUrl = fmt.Sprintf("%s %s", kubeConf.Cluster.ControlPlaneEndpoint.Address, fmt.Sprintf("api.%s", kubeConf.Cluster.Aicp.Domain))
+		if !strings.Contains(kubeConf.Cluster.DNS.DNSEtcHosts, aiUrl) {
+			kubeConf.Cluster.DNS.DNSEtcHosts = fmt.Sprintf("%s\n%s", aiUrl, kubeConf.Cluster.DNS.DNSEtcHosts)
+		}
+		if !strings.Contains(kubeConf.Cluster.DNS.DNSEtcHosts, apiUrl) {
+			kubeConf.Cluster.DNS.DNSEtcHosts = fmt.Sprintf("%s\n%s", apiUrl, kubeConf.Cluster.DNS.DNSEtcHosts)
+		}
+
+	}
+
+	if len(runtime.GetHostsByRole(common.Registry)) > 0 && kubeConf.Cluster.Registry.PrivateRegistry != "" {
+		registryUrl := fmt.Sprintf("%s %s", runtime.GetHostsByRole(common.Registry)[0].GetInternalIPv4Address(), kubeConf.Cluster.Registry.GetHost())
+		if !strings.Contains(kubeConf.Cluster.DNS.DNSEtcHosts, registryUrl) {
+			kubeConf.Cluster.DNS.DNSEtcHosts = fmt.Sprintf("%s\n%s", registryUrl, kubeConf.Cluster.DNS.DNSEtcHosts)
+		}
+	}
+
+	return kubeConf.Cluster.DNS.DNSEtcHosts
+}
